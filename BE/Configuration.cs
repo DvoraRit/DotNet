e67906b4896e9e561fab6a1597c8cc5eeea1fd82
DvoraRit <44133023+DvoraRit@users.noreply.github.com>
2021-05-19 08:36:51 +0300

    Add files via upload

---
+++ BE/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Configuration
+    {
+        public static int serial_Num_Of_Test_Id = 0000;
+        public static int min_lessons_to_be_tested = 20;
+        public static int max_age_for_tester = 60;
+        public static int min_age_for_tester = 40;
+        public static int min_age_for_trainee = 18;
+        
+
+    }
+}
---
+++ BE/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public enum Enum_Type_Of_Two_Weels_Car { motorcycle, Electric_Bicycle };
+    public enum Enum_Type_Of_Car {רכב_פרטי, רכב_דו_גלגלי, משאית_כבדה,משאית_בינונית};
+    public enum Enum_Type_Of_GearBox { ידנית,אוטומטית};
+    public enum Enum_Gender { נקבה,זכר};
+    public enum Enum_Success { NoData,עבר,נכשל};
+   
+}
